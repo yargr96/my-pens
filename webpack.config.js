dd7d3887c271a75a68568094e18b714543e5b7e5
Yaroslav Gromov <yargr96@gmail.com>
2023-03-12 23:27:24 +0600

    base webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash:8].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+    ],
+};
