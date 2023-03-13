d1205d2700e5bab67d34cfaba0d49b73ee6db5d7
Yaroslav Gromov <yargr96@gmail.com>
2023-03-13 13:27:26 +0600

    lint in progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
+    root: true,
+    env: {
+        node: true,
+    },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    root: true,
+    rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+    },
 };
