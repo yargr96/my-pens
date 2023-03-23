66124ccc61d1931a6c37e0f28069245392b90426
Yaroslav Gromov <yargr96@gmail.com>
2023-03-23 18:27:13 +0600

    coordinates base

--- src/modules/fractal-sets/FractalSets.ts
+++ src/modules/fractal-sets/FractalSets.ts
@@ -1,7 +1,14 @@
 import Canvas from '@/components/Canvas';
+import useCoordinates from '@/modules/fractal-sets/useCoordinates';
 import { Module } from '@/modules/moduleTypes';
 import colors from '@/styles/colors.module.scss';
-import { addVectors, multiplyVectorByNumber, Vector } from '@/utils/Vector';
+import { addVectors, Vector } from '@/utils/Vector';
+
+// z(n) = z(n-1)**2 + c
+const getComplexNumberSquare = ([x, y]: Vector): Vector => [
+    x ** 2 - y ** 2,
+    2 * x * y,
+];
 
 const FractalSets: Module = (mountElement) => {
     const {
@@ -22,30 +29,24 @@ const FractalSets: Module = (mountElement) => {
     const PADDING = 20;
 
     const coordinatesSquareSize = Math.min(canvas.width, canvas.height) - PADDING * 2;
+    const COORDINATE_SQUARE_MATH_SIZE = 4;
+    const pixelsPerOneMathCoordinate: number = coordinatesSquareSize / COORDINATE_SQUARE_MATH_SIZE;
+    const coordinatesCenter: Vector = [canvas.width / 2, canvas.height / 2];
 
-    const fieldZeroCoordinates: Vector = multiplyVectorByNumber([
-        canvas.width - coordinatesSquareSize,
-        canvas.height - coordinatesSquareSize,
-    ], 0.5);
-
-    const zeroCoordinates = addVectors(
-        fieldZeroCoordinates,
-        [coordinatesSquareSize / 2, coordinatesSquareSize / 2],
-    );
-
-    const canvasToFieldCoordinates = (canvasCoordinates: Vector): Vector => [
-        canvasCoordinates[0] - zeroCoordinates[0],
-        canvas.height - canvasCoordinates[1] - zeroCoordinates[1],
-    ];
+    const { getMathCoordinates, getCanvasCoordinates } = useCoordinates({
+        coordinatesCenter,
+        pixelsPerOneMathCoordinate,
+        canvas,
+    });
 
     context.fillStyle = colors.light;
-    context.fillRect(...fieldZeroCoordinates, coordinatesSquareSize, coordinatesSquareSize);
-
-    context.fillStyle = colors.dark;
-    context.fillRect(...addVectors(zeroCoordinates, [-5, -5]), 10, 10);
+    context.fillRect(...addVectors(coordinatesCenter, [-2, -2]), 4, 4);
 
     canvas.addEventListener('click', ({ offsetX, offsetY }) => {
-        console.log(canvasToFieldCoordinates([offsetX, offsetY]));
+        const mathCoordinates = getMathCoordinates([offsetX, offsetY]);
+        console.log(mathCoordinates);
+        console.log(getCanvasCoordinates(mathCoordinates));
+        console.log([offsetX, offsetY]);
     });
 
     return {};
---
+++ src/modules/fractal-sets/useCoordinates.ts
@@ -0,0 +1,55 @@
+import { multiplyVectorByNumber, Vector } from '@/utils/Vector';
+
+interface IUseCoordinatesProps {
+    coordinatesCenter: Vector;
+    pixelsPerOneMathCoordinate: number;
+    canvas: HTMLCanvasElement,
+}
+
+interface IUseCoordinates {
+    getMathCoordinates: (canvasCoordinates: Vector) => Vector;
+    getCanvasCoordinates: (mathCoordinates: Vector) => Vector;
+}
+
+const useCoordinates = ({
+    coordinatesCenter,
+    pixelsPerOneMathCoordinate,
+    canvas,
+}: IUseCoordinatesProps): IUseCoordinates => {
+    const canvasSize: Vector = [canvas.width, canvas.height];
+
+    const getShiftedCoordinates = (canvasCoordinates: Vector): Vector => [
+        canvasCoordinates[0] - coordinatesCenter[0],
+        canvasSize[1] - canvasCoordinates[1] - (canvasSize[1] - coordinatesCenter[1]),
+    ];
+
+    const getUnshiftedCoordinates = (shiftedCoordinates: Vector): Vector => [
+        shiftedCoordinates[0] + coordinatesCenter[0],
+        canvasSize[1] - shiftedCoordinates[1] - (canvasSize[1] - coordinatesCenter[1]),
+    ];
+
+    const getMathCoordinates = (canvasCoordinates: Vector): Vector => {
+        const shiftedCoordinates = getShiftedCoordinates(canvasCoordinates);
+
+        return [
+            shiftedCoordinates[0] / pixelsPerOneMathCoordinate,
+            shiftedCoordinates[1] / pixelsPerOneMathCoordinate,
+        ];
+    };
+
+    const getCanvasCoordinates = (mathCoordinates: Vector): Vector => {
+        const shiftedCoordinates = multiplyVectorByNumber(
+            mathCoordinates,
+            pixelsPerOneMathCoordinate,
+        );
+
+        return getUnshiftedCoordinates(shiftedCoordinates);
+    };
+
+    return {
+        getMathCoordinates,
+        getCanvasCoordinates,
+    };
+};
+
+export default useCoordinates;
