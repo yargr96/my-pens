f92c1e4f46918176df4c1eb3f8c7043847141ebc
Yaroslav Gromov <yargr96@gmail.com>
2023-03-24 18:15:25 +0600

    iterativeRender via render loop

--- src/modules/fractal-sets/iterativeRender.ts
+++ src/modules/fractal-sets/iterativeRender.ts
@@ -1,49 +1,45 @@
 import { Vector } from '@/utils/Vector';
+import getRenderLoop from '@/utils/useRenderLoop';
 
 interface IterativeRender {
     start: Vector;
     end: Vector;
     step?: number;
     callback: (coordinates: Vector, step: number) => void;
-    isRecursiveCall?: boolean;
 }
-
-let timerId: NodeJS.Timer;
 
 const iterativeRender = ({
     start,
     end,
     step = 8,
     callback,
-    isRecursiveCall = false,
 }: IterativeRender) => {
-    for (let x = start[0], i = 0; x < end[0]; x += step, i += 1) {
-        const isEvenColumn = i % 2 === 0;
+    let currentStep = step;
+    let isRecursiveCall = false;
 
-        const stepY = isEvenColumn && isRecursiveCall ? step * 2 : step;
-        const startY = isEvenColumn && isRecursiveCall
-            ? start[1] + step
-            : start[1];
+    const renderLoop = getRenderLoop(() => {
+        for (let x = start[0], i = 0; x < end[0]; x += currentStep, i += 1) {
+            const isEvenColumn = i % 2 === 0;
 
-        for (let y = startY; y < end[1]; y += stepY) {
-            callback([x, y], step);
+            const stepY = isEvenColumn && isRecursiveCall ? currentStep * 2 : currentStep;
+            const startY = isEvenColumn && isRecursiveCall
+                ? start[1] + currentStep
+                : start[1];
+
+            for (let y = startY; y < end[1]; y += stepY) {
+                callback([x, y], currentStep);
+            }
         }
-    }
 
-    if (step <= 1) {
-        return;
-    }
+        if (currentStep <= 1) {
+            renderLoop.stop();
+        }
 
-    clearTimeout(timerId);
-    timerId = setTimeout(() => {
-        iterativeRender({
-            start,
-            end,
-            step: step / 2,
-            callback,
-            isRecursiveCall: true,
-        });
-    }, 0);
+        currentStep /= 2;
+        isRecursiveCall = true;
+    });
+
+    renderLoop.run();
 };
 
 export default iterativeRender;
