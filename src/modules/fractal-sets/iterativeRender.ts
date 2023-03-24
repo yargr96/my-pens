1a9591a5d2e871877c02fa8c91f2dd5fe0b07726
Yaroslav Gromov <yargr96@gmail.com>
2023-03-24 16:05:37 +0600

    iterativeRender logic

--- src/modules/fractal-sets/iterativeRender.ts
+++ src/modules/fractal-sets/iterativeRender.ts
@@ -5,6 +5,7 @@ interface IterativeRender {
     end: Vector;
     step?: number;
     callback: (coordinates: Vector, step: number) => void;
+    isRecursiveCall?: boolean;
 }
 
 let timerId: NodeJS.Timer;
@@ -14,10 +15,18 @@ const iterativeRender = ({
     end,
     step = 8,
     callback,
+    isRecursiveCall = false,
 }: IterativeRender) => {
-    for (let i = start[0]; i < end[0]; i += step) {
-        for (let j = start[1]; j < end[1]; j += step) {
-            callback([i, j], step);
+    for (let x = start[0], i = 0; x < end[0]; x += step, i += 1) {
+        const isEvenColumn = i % 2 === 0;
+
+        const stepY = isEvenColumn && isRecursiveCall ? step * 2 : step;
+        const startY = isEvenColumn && isRecursiveCall
+            ? start[1] + step
+            : start[1];
+
+        for (let y = startY; y < end[1]; y += stepY) {
+            callback([x, y], step);
         }
     }
 
@@ -32,8 +41,9 @@ const iterativeRender = ({
             end,
             step: step / 2,
             callback,
+            isRecursiveCall: true,
         });
-    }, 100);
+    }, 0);
 };
 
 export default iterativeRender;
